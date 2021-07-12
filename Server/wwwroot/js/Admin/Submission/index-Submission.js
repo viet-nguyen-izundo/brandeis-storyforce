8d572248ea4430fea5972a86dfd51ec8226b0c2a
DESKTOP-LUHF0QE\hunga <khanh.nguyen@bmbsoft.com.vn>
2021-07-12 18:13:34 +0700

    task 49: refactor code

---
+++ Server/wwwroot/js/Admin/Submission/index-Submission.js
@@ -0,0 +1,19 @@
+﻿var indexSubmission = function () {
+
+    this.initialize = function () {
+        registerEvents();
+
+    };
+    function registerEvents() {
+
+    }
+
+
+};
+$(document).ready(function() {
+    $('.modal').modal();
+    $(".select2").select2({
+        dropdownAutoWidth: true,
+        width: '100%'
+    });
+});
